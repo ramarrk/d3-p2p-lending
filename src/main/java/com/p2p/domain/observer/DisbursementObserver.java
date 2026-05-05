6728d17e9e0abf697b0852dbf8223c64ccbcc99f
mond <ramadhank421@gmail.com>
2026-05-05 15:36:51 +0700

    feat: add Observer implementations (Lender, Borrower, Disbursement)

---
+++ src/main/java/com/p2p/domain/observer/DisbursementObserver.java
@@ -0,0 +1,13 @@
+package com.p2p.domain.observer;
+
+import com.p2p.domain.model.Loan;
+
+public class DisbursementObserver implements FundingObserver {
+
+    @Override
+    public void onFundingComplete(Loan loan) {
+        System.out.println("Disbursement triggered untuk loan "
+                + loan.getId()
+                + ": Proses pencairan dana dimulai.");
+    }
+}
