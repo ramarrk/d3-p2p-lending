6d058c9a906ebb169e6e6a02a3ae3e1343934ee3
mond <ramadhank421@gmail.com>
2026-05-13 21:34:12 +0700

    test: add unit tests for Funding domain model

--- src/test/java/com/p2p/domain/FundingTest.java
+++ src/test/java/com/p2p/domain/FundingTest.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FundingTest {
+public class FundingTest {
 
     @Test
     void shouldCreateFundingWithCorrectAttributes() {
