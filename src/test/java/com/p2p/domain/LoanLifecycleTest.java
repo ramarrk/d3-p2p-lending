aaa82814b9c3cdbf4f53a79db3d7bad66e1142af
mond <ramadhank421@gmail.com>
2026-05-21 11:01:06 +0700

    test: RED - shouldStartInPendingState

--- src/test/java/com/p2p/domain/LoanLifecycleTest.java
+++ src/test/java/com/p2p/domain/LoanLifecycleTest.java
@@ -1,12 +1,10 @@
 package com.p2p.domain;
-
 import com.p2p.domain.model.Loan;
 import com.p2p.domain.state.PendingState;
 import com.p2p.domain.valueobject.Money;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
-
 class LoanLifecycleTest {
 
     @Test
