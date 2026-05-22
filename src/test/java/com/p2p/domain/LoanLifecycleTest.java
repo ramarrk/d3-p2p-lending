0dafb25f9bc54d19124a4ca27441e57ad887ba81
mond <ramadhank421@gmail.com>
2026-05-22 09:05:43 +0700

    test: [RED] cek state awal harus PendingState

--- src/test/java/com/p2p/domain/LoanLifecycleTest.java
+++ src/test/java/com/p2p/domain/LoanLifecycleTest.java
@@ -1,7 +1,7 @@
 package com.p2p.domain;
 
 import com.p2p.domain.model.Loan;
-import com.p2p.domain.state.PendingState;
+import com.p2p.domain.state.*;
 import com.p2p.domain.valueobject.Money;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
@@ -11,7 +11,6 @@ class LoanLifecycleTest {
     @Test
     void shouldStartInPendingState() {
         Loan loan = new Loan("L001");
-        loan.setTargetAmount(new Money(new BigDecimal("10000000")));
         assertInstanceOf(PendingState.class, loan.getState());
     }
 }
