2b839041e4f1034a8e0036b3e75ce3bd5385d796
t_____p's belonging <toiletpaper@t-ps-mac.local>
2026-06-04 00:58:25 +0700

    menyelesaikan fitur user risk

--- src/main/java/com/p2p/domain/risk/CreditScoreHandler.java
+++ src/main/java/com/p2p/domain/risk/CreditScoreHandler.java
@@ -5,11 +5,18 @@ import com.p2p.domain.model.Borrower;
 import com.p2p.domain.valueobject.Money;
 
 public class CreditScoreHandler extends RiskHandler {
+
     @Override
-    public void handle(Borrower borrower, Money loanAmount) {
+    public void handle(
+            Borrower borrower,
+            Money loanAmount) {
+
         if (borrower.getCreditScore() < 600) {
             throw new InsufficientCreditScoreException();
         }
-        if (next != null) next.handle(borrower, loanAmount);
+
+        if (next != null) {
+            next.handle(borrower, loanAmount);
+        }
     }
 }
