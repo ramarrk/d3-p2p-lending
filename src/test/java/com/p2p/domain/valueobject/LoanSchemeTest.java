682922238bd2447a3e8c77841e7e78a1b3a9b3ac
mond <ramadhank421@gmail.com>
2026-06-04 02:01:19 +0700

    test: validate LoanScheme value object logic

---
+++ src/test/java/com/p2p/domain/valueobject/LoanSchemeTest.java
@@ -0,0 +1,16 @@
+package com.p2p.domain.valueobject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoanSchemeTest {
+
+    @Test
+    public void testLoanSchemeValues() {
+        LoanScheme[] schemes = LoanScheme.values();
+        assertTrue(schemes.length > 0);
+
+        String firstName = schemes[0].name();
+        assertNotNull(LoanScheme.valueOf(firstName));
+    }
+}
