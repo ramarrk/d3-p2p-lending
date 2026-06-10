ba28caef7b8333efc8724d7fd7b9f12820d0607c
mond <ramadhank421@gmail.com>
2026-06-10 20:37:21 +0700

    test: [RED] uji validasi angka negatif pada Money

---
+++ src/test/java/com/p2p/domain/valueobject/MoneyTest.java
@@ -0,0 +1,15 @@
+package com.p2p.domain.valueobject;
+
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MoneyTest {
+
+    @Test
+    void shouldThrowExceptionWhenAmountIsNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Money(new BigDecimal("-1000"));
+        });
+    }
+}
