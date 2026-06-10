5d469fe150e0f02acb26cc36957d16daca611bbc
mond <ramadhank421@gmail.com>
2026-06-11 06:38:59 +0700

    feat: green - InterestRate throws exception when value less than zero

--- src/main/java/com/p2p/domain/valueobject/InterestRate.java
+++ src/main/java/com/p2p/domain/valueobject/InterestRate.java
@@ -3,4 +3,12 @@ package com.p2p.domain.valueobject;
 import java.math.BigDecimal;
 
 public class InterestRate {
+    private final BigDecimal value;
+
+    public InterestRate(BigDecimal value) {
+        if (value.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException("Rate tidak boleh kurang dari 0");
+        this.value = value;
+    }
+
 }
