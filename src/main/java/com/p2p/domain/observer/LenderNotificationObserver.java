ad7cde126427972a5b81b0f447d9fd4bf7a80c11
mond <ramadhank421@gmail.com>
2026-06-11 12:36:58 +0700

    refactor: replace System.out with logger in LenderNotificationObserver

--- src/main/java/com/p2p/domain/observer/LenderNotificationObserver.java
+++ src/main/java/com/p2p/domain/observer/LenderNotificationObserver.java
@@ -2,12 +2,15 @@ package com.p2p.domain.observer;
 
 import com.p2p.domain.model.Funding;
 import com.p2p.domain.model.Loan;
+import java.util.logging.Logger;
 
 public class LenderNotificationObserver implements FundingObserver {
+    private static final Logger logger = Logger.getLogger(LenderNotificationObserver.class.getName());
+
     @Override
     public void onFundingComplete(Loan loan) {
         for (Funding funding : loan.getFundings()) {
-            System.out.println("Notifikasi ke lender " + funding.getLenderId()
+            logger.info("Notifikasi ke lender " + funding.getLenderId()
                     + ": Loan " + loan.getId() + " telah penuh didanai!");
         }
     }
