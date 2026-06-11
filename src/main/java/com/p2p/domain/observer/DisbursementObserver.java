22cd3827cf722b301e90e4bc08c6fde9b85c66fa
mond <ramadhank421@gmail.com>
2026-06-11 07:51:35 +0700

    feat: green - DisbursementObserver prints loan ID

--- src/main/java/com/p2p/domain/observer/DisbursementObserver.java
+++ src/main/java/com/p2p/domain/observer/DisbursementObserver.java
@@ -1,4 +1,12 @@
 package com.p2p.domain.observer;
 
+import com.p2p.domain.model.Loan;
+
 public class DisbursementObserver implements FundingObserver {
+    @Override
+    public void onFundingComplete(Loan loan) {
+        System.out.println("Disbursement triggered untuk loan "
+                + loan.getId()
+                + ": Proses pencairan dana dimulai.");
+    }
 }
