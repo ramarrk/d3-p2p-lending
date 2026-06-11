f93f58ad17431f9ee017fd4e7bc361e817b0b7dd
mond <ramadhank421@gmail.com>
2026-06-11 07:00:17 +0700

    test: red - LoanStatus should have all required values

--- src/main/java/com/p2p/domain/valueobject/LoanStatus.java
+++ src/main/java/com/p2p/domain/valueobject/LoanStatus.java
@@ -1,12 +1,4 @@
 package com.p2p.domain.valueobject;
 
 public enum LoanStatus {
-    PENDING,
-    APPROVED,
-    REJECTED,
-    FUNDING,
-    DISBURSED,
-    REPAYMENT,
-    CLOSED,
-    CANCELLED
 }
