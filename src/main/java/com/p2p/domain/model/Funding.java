5c7347796b062fa0ef21a0f786d0bbc56354b0b7
mond <ramadhank421@gmail.com>
2026-06-11 09:21:20 +0700

    test: red - Funding should return correct id

--- src/main/java/com/p2p/domain/model/Funding.java
+++ src/main/java/com/p2p/domain/model/Funding.java
@@ -1,4 +1,13 @@
 package com.p2p.domain.model;
 
 public class Funding {
+    private final String id;
+
+    public Funding(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
