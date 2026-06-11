af3d38b35138e9a43ac44da1edd036996cd3da54
mond <ramadhank421@gmail.com>
2026-06-11 09:29:07 +0700

    feat: green - Funding returns correct loanId

--- src/main/java/com/p2p/domain/model/Funding.java
+++ src/main/java/com/p2p/domain/model/Funding.java
@@ -2,12 +2,13 @@ package com.p2p.domain.model;
 
 public class Funding {
     private final String id;
+    private final String loanId;
 
-    public Funding(String id) {
+    public Funding(String id, String loanId) {
         this.id = id;
+        this.loanId = loanId;
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
+    public String getLoanId() { return loanId; }
 }
