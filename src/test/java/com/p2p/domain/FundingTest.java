283ee3e067a00691dec9013684ada010dca49f41
mond <ramadhank421@gmail.com>
2026-06-11 09:41:37 +0700

    test: red - Loan should add funding and update total funded

--- src/test/java/com/p2p/domain/FundingTest.java
+++ src/test/java/com/p2p/domain/FundingTest.java
@@ -5,6 +5,7 @@ import com.p2p.domain.valueobject.Money;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
+import com.p2p.domain.model.Loan;
 
 public class FundingTest {
     @Test
@@ -37,5 +38,22 @@ public class FundingTest {
         Money amount = new Money(new BigDecimal("5000000"));
         Funding funding = new Funding("F001", "L001", "LN001", amount);
         assertNotNull(funding.getFundedAt());
+    }
+
+    RED
+    java@Test
+    void shouldAddFundingAndUpdateTotalFunded() {
+        Loan loan = new Loan("L001");
+        loan.approve();
+        loan.startFunding();
+        loan.setTargetAmount(new Money(new BigDecimal("10000000")));
+
+        loan.addFunding(new Funding("F001", "L001", "LN001",
+                new Money(new BigDecimal("5000000"))));
+
+        assertEquals(
+                new BigDecimal("5000000").stripTrailingZeros(),
+                loan.getTotalFunded().getAmount().stripTrailingZeros()
+        );
     }
 }
