ffd95b45927252280a463f19136082e866416ffb
mond <ramadhank421@gmail.com>
2026-06-11 11:38:08 +0700

    test: red - InMemoryLenderRepository should find all lenders

--- src/test/java/com/p2p/infrastructure/repository/InMemoryLenderRepositoryTest.java
+++ src/test/java/com/p2p/infrastructure/repository/InMemoryLenderRepositoryTest.java
@@ -25,5 +25,18 @@ public class InMemoryLenderRepositoryTest {
 
         assertTrue(repository.findById("LN001").isPresent());
     }
-    
+
+    @Test
+    void shouldFindAllLenders() {
+        Lender lender1 = new Lender("LN001", "John", "john@mail.com",
+                new Money(new BigDecimal("10000000")));
+        Lender lender2 = new Lender("LN002", "Jane", "jane@mail.com",
+                new Money(new BigDecimal("5000000")));
+        repository.save(lender1);
+        repository.save(lender2);
+
+        List<Lender> result = repository.findAll();
+
+        assertEquals(2, result.size());
+    }
 }
